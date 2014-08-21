2d0a0ac313a7dcf666c6f2e299e2ff164d1e638e
Björn Klose <mail@bjoernklose.de>
2014-08-21 08:55:40 +0200

    basic version

---
+++ README.md
@@ -0,0 +1,11 @@
+# enable CORS for the new Wordpress JSON API
+
+until https://github.com/WP-API/WP-API/issues/144 is fixed, here is a standalone plugin that will enable CORS
+
+You might want to restrict responses by running a string comparison of your get_http_origin() result.
+
+## Usage
+
+1. `cd wp-content/plugins`
+2. `git clone https://github.com/bjoernklose/wordpress-api-cors.git wp-json-api-cors`
+3. (if you have the excellent wp-cli installed) `wp plugin activate wp-json-api-cors`
---
+++ wp-json-api-cors.php
@@ -0,0 +1,33 @@
+﻿<?php
+/*/
+Plugin Name: wp-json-api-cors
+Plugin URI: https://github.com/bjoernklose/wordpress-api-cors
+Description: enable CORS for the new JSON Wordpress API (temporary solution)
+Version: 0.1
+Author: Bjoern Klose
+Author URI: https://github.com/bjoernklose
+/*/
+
+/**
+ * enable CORS for WP-API
+ * until
+ * https://github.com/WP-API/WP-API/issues/144
+ * is fixed
+ */
+
+add_action( 'init', 'handle_preflight' );
+
+function handle_preflight() {
+    header("Access-Control-Allow-Origin: " . get_http_origin());
+    header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
+    header("Access-Control-Allow-Credentials: true");
+    header("Access-Control-Allow-Headers: Authorization, Origin, X-Requested-With, Content-Type, Accept");
+
+    if ( 'OPTIONS' == $_SERVER['REQUEST_METHOD'] ) {
+        status_header(200);
+        exit();
+    }
+}
+
+
+
